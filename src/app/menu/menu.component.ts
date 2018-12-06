2be0a3a95702d839257231139bd2131661ed654e
jgolmard <jgolmard@e2.fdsetu.infra.umontpellier.fr>
2018-12-06 14:16:42 +0100

    Change navbar, now dynamic

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, DoCheck {
 
   private login: string;
   private isLogged: boolean = false;
   constructor() { }
 
   ngOnInit() {
-  	this.login = localStorage.getItem('user');
-  	if(this.login != null)
-  		this.isLogged=true;
-
+  }
+  ngDoCheck(){
+    	this.login = localStorage.getItem('user');
+  		if(this.login != null){
+  			this.isLogged=true;	
+  		}
+  		console.log(this.login);
   }
   logOut(){
   	this.isLogged = false;
