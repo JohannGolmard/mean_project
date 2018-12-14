06be5bce267186579ff5ef9aeb974db34d5809b7
CedricDurand <cedricdurand13@sfr.fr>
2018-12-14 22:43:22 +0100

    Ajout formulaire ajout Service

--- src/app/users/profil-user/profil-user.component.ts
+++ src/app/users/profil-user/profil-user.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 export class ProfilUserComponent implements OnInit {
 
   private afficherFormBien : boolean =false; // false pour cacher true pour afficher
-  private afficherFormService : boolean =false;
+  private afficherFormService : boolean =true;
 
   //formulaire bien
   private nomBien : string;
@@ -18,6 +18,14 @@ export class ProfilUserComponent implements OnInit {
   private prix: number =1;
   private tags_bien: Array<number>=[];
   private amBien: boolean =true;
+
+  //formulaire service
+  private titreService : string;
+  private descService :string;
+  private experience : string;
+  private prixService: number =1;
+  private tags_service: Array<number>=[];
+  private amService: boolean =true;
 
   //info utilisateur
   private nom: string;
@@ -28,13 +36,20 @@ export class ProfilUserComponent implements OnInit {
   private services : Object[] = [];
 
   private les_tags : Object[];
+  private les_tags_autre : Object[];
+
   private les_tags_biens_name : Object[] =[];
   private les_tags_services_name : Object[];
   private selected_tag;
+  private selected_tag_autre;
 
   private la_date_choisi;
+  private la_date_choisi_autre;
   private date_biens : Object[] =[];
   private date_biens_string :string ="";
+
+  private date_services : Object[] =[];
+  private date_services_string :string ="";
 
   constructor(private service: UsersService,private router: Router) { }
 
@@ -55,10 +70,14 @@ export class ProfilUserComponent implements OnInit {
 				this.service.addDispo(data).subscribe(res =>{});
 			}
 		  	this.update();
-  			this.reset();
+  			this.resetBien();
 		});
   	}
 
+  }
+
+  addService(){
+    	this.resetService();
   }
 
   formBien(){
@@ -91,12 +110,13 @@ export class ProfilUserComponent implements OnInit {
 	           this.services = res;
 	           this.service.getTag().subscribe(res => {
 	           		this.les_tags=res;
+	           		this.les_tags_autre=res;
 	           });
 	        });
   		});
   }
 
-  reset(){
+  resetBien(){
   	  this.date_biens_string = "";
 	  this.nomBien="";
 	  this.descriptif="";
@@ -104,11 +124,27 @@ export class ProfilUserComponent implements OnInit {
 	  this.amBien=true;
 	  this.tags_bien=[];
 	  this.les_tags_biens_name=[];
-	  this.les_tags_services_name=[];
 	  this.selected_tag=[];
 
 	  this.la_date_choisi = [];
+	  this.la_date_choisi_autre=[];
 	  this.date_biens=[];
+	  this.date_services=[];
+
+  }
+
+   resetService(){
+  		this.la_date_choisi_autre="";
+   		this.date_services_string="";
+   		this.titreService="";
+		this.descService="";
+		this.experience="";
+		this.prixService=1;
+		this.tags_service=[];
+		this.amService=true;
+		this.les_tags_services_name=[];
+
+		this.selected_tag_autre=[];
 
   }
 
@@ -136,6 +172,33 @@ export class ProfilUserComponent implements OnInit {
   		this.date_biens.push(date);
 	  	this.date_biens_string+=date+"\n";
   	}
+  }
+
+  ajoutTagService(){
+    if(this.selected_tag_autre!=undefined){
+	  	let un_tab = new Array;
+	  	for(let i =0; i<this.selected_tag_autre.length;i++){
+	  		this.tags_bien.push(this.selected_tag_autre[i].idTags);
+	  		un_tab.push(this.selected_tag_autre[i].nom)
+	  	}
+	  	this.les_tags_services_name=un_tab;
+  	}
+
+  }
+
+  ajoutDateService(){
+	  if(this.la_date_choisi_autre!=undefined){
+	  		let amOrPm = "am";
+	  		let jour = this.la_date_choisi_autre.day;
+	  		let mois = this.la_date_choisi_autre.month;
+	  		let annee = this.la_date_choisi_autre.year;
+	  		if(this.amService==false)
+	  			amOrPm="pm";
+
+	  		let date = jour+"/"+mois+"/"+annee+":"+amOrPm;
+	  		this.date_services.push(date);
+		  	this.date_services_string+=date+"\n";
+	  	}
   }
 
   update(){
