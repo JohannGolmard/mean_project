f26cd904eeedf0827c782ac7bd982ffcee9f5b9a
golmard <johann.golmard@iut-valence.fr>
2018-12-15 14:36:58 +0100

    Ajout des suppléments de page service dans page bien

--- src/app/biens/page-bien/page-bien.component.ts
+++ src/app/biens/page-bien/page-bien.component.ts
@@ -11,6 +11,7 @@ export class PageBienComponent implements OnInit {
   
   private bien : any;
   private tags : string = "";
+  private dispo : boolean = true;
 
   constructor(private service : SearchBiensService, private route : Router) { }
 
@@ -23,6 +24,17 @@ export class PageBienComponent implements OnInit {
   		for(let tag of this.bien.tags){
   			this.tags+=tag.nom+" ";
   		}
+  	}
+  	if(this.bien.utilisations.length != 0){
+	  	let date = this.bien.disponibilites[0].date;
+	  	let ap = this.bien.disponibilites[0].AMPM;
+	  	let used = this.bien.utilisations[0];
+	  	console.log(date+" "+ap+ " "+used);
+	  	if(used.date === date){
+	  		if(used.AMPM === ap){
+	  			this.dispo = false;
+	  		}
+	  	}
   	}
   }
 
